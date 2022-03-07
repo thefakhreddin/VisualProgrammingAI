38778c5e3491d4bd658e5aa79c28055e3d11732a
thefakhreddin <fakhredin0098@gmail.com>
2022-03-07 13:23:10 +0330

    sample images added

--- app/src/main/java/com/example/visualprogramming/MainActivity.kt
+++ app/src/main/java/com/example/visualprogramming/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.visualprogramming
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,12 +10,22 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val MAX_FONT_SIZE = 70F
+    }
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
-    private var currentImagePath: String? = null
 
     private lateinit var btnOpenCamera: Button
+    private lateinit var btnSampleImage: Button
     private lateinit var ivPhoto: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnOpenCamera = findViewById(R.id.btnOpenCamera)
+        btnSampleImage = findViewById(R.id.btnSampleImage)
         ivPhoto = findViewById(R.id.ivImage)
 
         resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -31,6 +42,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+//        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        resultLauncher.launch(cameraIntent)
 
 
         btnOpenCamera.setOnClickListener {
@@ -38,12 +51,97 @@ class MainActivity : AppCompatActivity() {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(cameraIntent)
         }
+
+        btnSampleImage.setOnClickListener {
+            setViewAndDetect(getSampleImage(R.drawable.sample_2))
+        }
     }
 
 
     private fun handleCameraImage(intent: Intent?) {
         val bitmap = intent?.extras?.get("data") as Bitmap
-        ivPhoto.setImageBitmap(bitmap)
+        setViewAndDetect(bitmap)
+    }
 
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        ivPhoto.setImageBitmap(bitmap)
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+    private fun runObjectDetection(bitmap: Bitmap) {
+        val image = TensorImage.fromBitmap(bitmap)
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+            .setMaxResults(50)
+            .setScoreThreshold(0.3f)
+            .build()
+        val detector = ObjectDetector.createFromFileAndOptions(
+            this, // the application context
+            "model.tflite", // must be same as the filename in assets folder
+            options
+        )
+        val results = detector.detect(image)
+        val resultToDisplay = results.map {
+            // Get the top-1 category and craft the display text
+            val category = it.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            // Create a data object to display the detection result
+            DetectionResult(it.boundingBox, text)
+        }
+        // Draw the detection result on the bitmap and show it.
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            ivPhoto.setImageBitmap(imgWithResult)
+        }
+    }
+
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 4F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 1F
+
+            pen.textSize = MAX_FONT_SIZE
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
+    }
+
+    private fun getSampleImage(drawable: Int): Bitmap {
+        return BitmapFactory.decodeResource(resources, drawable, BitmapFactory.Options().apply {
+            inMutable = true
+        })
     }
 }
+
+data class DetectionResult(val boundingBox: RectF, val text: String)
